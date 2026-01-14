def522e904fada280a9381c02e5ab609595ebf8b
David <davidpayala@outlook.com>
2026-01-14 12:09:24 -0500

    separacion de codigo en archivos pestañas - correcion 7

---
+++ views/__init__.py
@@ -0,0 +1,10 @@
+# views/__init__.py
+
+from . import ventas
+from . import compras
+from . import inventario
+from . import clientes
+from . import seguimiento
+from . import catalogo
+from . import facturacion
+from . import chat
